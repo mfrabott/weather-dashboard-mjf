2ff3baedb7f8d638a3b071e47c96a562f84d7cfe
mfrabott <mfrabott14@gmail.com>
2022-11-05 15:29:48 -0400

    add geocode and 5day forecast APIs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,32 +3,36 @@ var fetchButton = document.querySelector('#fetch-button');
 
 var latInput = '44.34';
 var lonInput = '-10.99';
-var apiKey = 'bfaa3809424ecd171e99f41a51df3779';
+var apiKey = '99afeda6d0c563b7397dc3f6cc43fdd6';
 
-function getApi() {
-  var requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=' + apiKey;
+
+// Geocode API
+function getCoordinatesApi() {
+      var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityInput + ',' + {stateCode} + ',' + {countryCode} + '&limit=' + {limit} + '&appid=' + apiKey;
+      console.log(requestUrl)
+      fetch(requestUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+        console.log(data);
+        });
+    };
+
+
+// 5-day API
+function getFiveDayApi() {
+//   var requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=-10.99&appid=' + apiKey;
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latInput + '&lon=' + lonInput + '&appid=' + apiKey;
   console.log(requestUrl)
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      //Using console.log to examine the data
-      console.log(data);
-    //   for (var i = 0; i < data.length; i++) {
-        //Creating a h3 element and a p element
-        // var userName = document.createElement('h3');
-        // var userUrl = document.createElement('p');
-
-        // //Setting the text of the h3 element and p element.
-        // userName.textContent = data[i].login;
-        // userUrl.textContent = data[i].url;
-
-        // //Appending the dynamically generated html to the div associated with the id="users"
-        // //Append will attach the element as the bottom most child.
-        // usersContainer.append(userName);
-        // usersContainer.append(userUrl);
-    //   }
+    console.log(data);
     });
-}
-fetchButton.addEventListener('click', getApi);
+};
+
+fetchButton.addEventListener('click', getCoordinatesApi);
+
