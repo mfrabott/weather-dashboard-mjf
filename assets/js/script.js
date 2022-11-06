e10398e9685b9edeb5927eaded9ee5045539e8cd
mfrabott <mfrabott14@gmail.com>
2022-11-05 21:38:46 -0400

    organize logic structure in js and set up endpoints

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,65 @@
 var usersContainer = document.getElementById('users');
 var fetchButton = document.querySelector('#fetch-button');
 
-var latInput = '44.34';
-var lonInput = '-10.99';
+var citySelector = document.querySelector('#cityInput');
+
+var selectedCity = "";
+
 var apiKey = '99afeda6d0c563b7397dc3f6cc43fdd6';
 
 
+
+// 5-day API
+function getFiveDayApi(latitude, longitude) {
+//   var requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=-10.99&appid=' + apiKey;
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+  console.log(requestUrl)
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (fiveDayData) {
+    console.log(fiveDayData);
+    var hourlyForecast = fiveDayData.list;
+    console.log(hourlyForecast);
+    for (k=0; k<fiveDayData.list.length; k++){
+        console.log(fiveDayData.list.length)
+        console.log(fiveDayData.list[k].dt_txt)
+        console.log(fiveDayData.list[k].weather[0].main)
+    }
+    });
+};
+
+
 // Geocode API
-function getCoordinatesApi() {
-      var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityInput + ',' + {stateCode} + ',' + {countryCode} + '&limit=' + {limit} + '&appid=' + apiKey;
+function getCoordinatesApi(city) {
+      var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + apiKey;
+    //   var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + selectedCity + ',' + {stateCode} + ',' + {countryCode} + '&limit=' + {limit} + '&appid=' + apiKey;
       console.log(requestUrl)
       fetch(requestUrl)
         .then(function (response) {
           return response.json();
         })
         .then(function (data) {
-        console.log(data);
+            console.log(data);
+            var latitude = data[0].lat;
+            var longitude = data[0].lon;
+            getFiveDayApi(latitude, longitude)
         });
+        
     };
 
 
-// 5-day API
-function getFiveDayApi() {
-//   var requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=-10.99&appid=' + apiKey;
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latInput + '&lon=' + lonInput + '&appid=' + apiKey;
-  console.log(requestUrl)
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-    console.log(data);
-    });
-};
+//checks whether the pressed key is "Enter"
+citySelector.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {  
+        var selectedCity = e.target.value;
+        console.log(selectedCity);
+        getCoordinatesApi(selectedCity);
+    }
+});
 
-fetchButton.addEventListener('click', getCoordinatesApi);
 
+// function validate(e) {
+
+// fetchButton.addEventListener('click', getCoordinatesApi);
